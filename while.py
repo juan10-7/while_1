03a6ce800af9285aac367dba2e7a7f7f3163f355
juan10-7 <juansantana1@colegioguanenta.edu.co>
2025-02-13 18:15:29 -0500

    se agregaron nuevos archivos

--- while.py
+++ while.py
@@ -1,6 +1,20 @@
 # input
 
-n=int(input("dijite l"))
+n=int(input("dijite el valor de n: "))
+
+
+# processing
+
+s=0
+
+i=1
+
+while i<=n:
+    s=s+i
+    i=i+1
+
+# output
+print("la suma de los "+str(n)+ "primeros numeros naturales es "+ str(s))
 
 
 
